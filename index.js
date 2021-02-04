1fb87ee8231045634bd6be821718c4cfa67efa28
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-02-03 21:31:28 -0500

    add pop-up button with v-on:click

--- index.js
+++ index.js
@@ -5,13 +5,23 @@ var app = new Vue({
   data: function () {
     return {
       message: "Hello from JavaScript!",
-      seen: true,
-      todos: [{ text: "Learn JavaScript" }, { text: "Learn Vue" }, { text: "Build something awesome" }],
+      // seen: true,
+      // todos: [{ text: "Learn JavaScript" }, { text: "Learn Vue" }, { text: "Build something awesome" }],
+      name: "Vue.js",
     };
   },
   methods: {
     reverseMessage: function () {
+      console.log("reverseMessage", this.message);
       this.message = this.message.split("").reverse().join("");
+    },
+    greet: function (event) {
+      // `this` inside methods points to the Vue instance
+      alert("Hello " + this.name + "!");
+      // `event` is the native DOM event
+      if (event) {
+        alert(event.target.tagName);
+      }
     },
   },
 });
